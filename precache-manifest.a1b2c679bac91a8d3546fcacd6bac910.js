745c7216d25e553b3545329858d38683931d0370
andronikosf <sdi1400214@di.uoa.gr>
2020-02-24 16:36:00 +0200

    Updates

---
+++ precache-manifest.a1b2c679bac91a8d3546fcacd6bac910.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d6f99095b7cca78ed9f05468b0cdf62",
+    "url": "/monsters-roledex/index.html"
+  },
+  {
+    "revision": "0d3e7b1dbf2a9f7e73c3",
+    "url": "/monsters-roledex/static/css/main.aa945937.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-roledex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-roledex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d3e7b1dbf2a9f7e73c3",
+    "url": "/monsters-roledex/static/js/main.92c904d6.chunk.js"
+  },
+  {
+    "revision": "58f2a2989006c8fcd4bc",
+    "url": "/monsters-roledex/static/js/runtime-main.c1b7bf5e.js"
+  }
+]);
